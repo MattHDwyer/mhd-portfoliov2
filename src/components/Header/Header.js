674dda06dc1cc637ee66231dc93c50e22be221cc
MattHDwyer <matthew.h.dwyer@gmail.com>
2021-03-30 21:44:15 +1100

    update to push header to be a sidebar nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Header.css'
+import { FaShopify, FaBrain, FaBookReader, FaEnvelope } from "react-icons/fa";
 
 const Header = () => {
 	return (
 		<header className="headerContainer">
-			<h3 className="headerItem">Matt Dwyer | Web Developer & Designer</h3>
+			{/* <h3 className="headerItem">Matt Dwyer | Web Developer & Designer</h3> */}
 			<nav className="navContainer">
 				<ul className="navbar">
+					<li className="navItem"><a className="navLink" href="/">LOGO</a></li>
+					<li className="navItem"><a className="navLink" href="/about"><FaBookReader />About</a></li>
+					<li className="navItem"><a className="navLink" href="/skills"><FaBrain />Skills</a></li>
+					<li className="navItem"><a className="navLink" href="/shopify-portfolio"><FaShopify /> Portfolio</a></li>
+					<li className="navItem"><a className="navLink" href="/contact"><FaEnvelope />Contact</a></li>
 					<li className="navItem"><a className="navLink" href="/">Home</a></li>
-					<li className="navItem"><a className="navLink" href="/about">About</a></li>
-					<li className="navItem"><a className="navLink" href="/skills">Skills</a></li>
-					<li className="navItem"><a className="navLink" href="/shopify-portfolio">Shopify Portfolio</a></li>
-					<li className="navItem"><a className="navLink" href="/contact">Contact</a></li>
 				</ul>
 			</nav>
 		</header>
