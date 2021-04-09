9a2949b0681df1d93b72c5deb58a3ef607e121d9
MattHDwyer <matthew.h.dwyer@gmail.com>
2021-04-09 17:09:00 +1000

    changed home page text

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -7,7 +7,6 @@ function Index() {
 	return (
 		<div>
 			<h1>Matthew Dwyers Portfolio</h1>
-			<p style={{ textAlign: 'center' }}>This site is currently a work in progress. Please forgive any lack of information or current bugs.</p>
 			<div className="indexContainer">
 				<About />
 				<Skills />
