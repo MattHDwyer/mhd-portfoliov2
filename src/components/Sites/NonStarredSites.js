ce44bd44783ccc344cb7b79b1a3e6ef095e64e6b
MattHDwyer <matthew.h.dwyer@gmail.com>
2021-04-09 16:33:03 +1000

    changes to the non-starred sites component

--- src/components/Sites/NonStarredSites.js
+++ src/components/Sites/NonStarredSites.js
@@ -1,6 +1,6 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React/*, { useLayoutEffect, useState }*/ from 'react';
 import './NonStarredSites.css'
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
 import sitesData from '../../data/shopifySites.json';
 
 const Site = () => {
@@ -31,18 +31,18 @@ const Site = () => {
 	// Execute the nonStarredSites function
 	nonStarredSitesFill();
 
-	function useWindowSize() {
-		const [size, setSize] = useState([0, 0]);
-		useLayoutEffect(() => {
-			function updateSize() {
-				setSize([window.innerWidth, window.innerHeight]);
-			}
-			window.addEventListener('resize', updateSize);
-			updateSize();
-			return () => window.removeEventListener('resize', updateSize);
-		}, []);
-		return size;
-	}
+	// function useWindowSize() {
+	// 	const [size, setSize] = useState([0, 0]);
+	// 	useLayoutEffect(() => {
+	// 		function updateSize() {
+	// 			setSize([window.innerWidth, window.innerHeight]);
+	// 		}
+	// 		window.addEventListener('resize', updateSize);
+	// 		updateSize();
+	// 		return () => window.removeEventListener('resize', updateSize);
+	// 	}, []);
+	// 	return size;
+	// }
 
 	// function ShowWindowDimensions() {
 	// 	const [width, height] = useWindowSize();
@@ -50,120 +50,44 @@ const Site = () => {
 	// }
 
 
-	let screenWidth = 4;
-	let maxLength = 150;
 
-	const SetScreenWidth = (props) => {
-		const [width] = useWindowSize();
-		if (width >= 0 && width <= 400) {
-			screenWidth = 1;
-			maxLength = 400;
+	// const SetScreenWidth = (props) => {
+	// 	const [width] = useWindowSize();
+	// 	if (width >= 0 && width <= 400) {
+	// 		screenWidth = 1;
+	// 		maxLength = 400;
 
-		} else if (width >= 401 && width <= 900) {
-			screenWidth = 2;
-			maxLength = 150;
+	// 	} else if (width >= 401 && width <= 900) {
+	// 		screenWidth = 2;
+	// 		maxLength = 150;
 
-		} else if (width >= 901 && width <= 1100) {
-			screenWidth = 3;
-			maxLength = 50;
+	// 	} else if (width >= 901 && width <= 1100) {
+	// 		screenWidth = 3;
+	// 		maxLength = 50;
 
-		} else if (width >= 1101 && width <= 1400) {
-			screenWidth = 3;
-			maxLength = 150;
+	// 	} else if (width >= 1101 && width <= 1400) {
+	// 		screenWidth = 3;
+	// 		maxLength = 150;
 
-		} else if (width > 1400) {
-			screenWidth = 4;
-			maxLength = 250;
-		}
-	}
-	SetScreenWidth();
+	// 	} else if (width > 1400) {
+	// 		screenWidth = 4;
+	// 		maxLength = 250;
+	// 	}
+	// }
+	// SetScreenWidth();
 
 
-	const [modalIsOpen, setIsOpen] = useState(-10);
-
-	const getModal = (index) => {
-		setIsOpen(index);
-	}
-
-	const closeModal = () => {
-		setIsOpen(-10);
-	}
 
 
 	return (
 		<div className="nonStarredSitesSectionContainer">
 
 			{nonStarredSites.map((siteDetail, index) => {
-
-				return (<div className="nonStarredSiteItem" id={siteDetail.id} key={index}>
-					{((index + 1) / screenWidth) % 1 === 0 ?
-						<div className="lastColumn">
-							<img className="nonStarredScreenshot" src={siteDetail.screenshotUrl} alt={siteDetail.name} />
-							{/* <div className="onHoverInfo">
-								<h3 className="slideOutTitle">{siteDetail.name}</h3>
-								<div className="nonStarredFeatures">
-									{siteDetail.features !== null &&
-										<>
-											{siteDetail.features.length > maxLength ?
-												<>
-													<p>{`${siteDetail.features.substring(0, maxLength)}...`}<br /> <button onClick={() => { getModal(index) }}>Read more</button></p>
-													<Modal
-														isOpen={modalIsOpen === index}
-														onRequestClose={closeModal}
-														ariaHideApp={false}
-													>
-														<h3>{siteDetail.name} </h3>
-														<p>{siteDetail.features}</p>
-														<button className="modalClose" onClick={() => { closeModal() }}>Close</button>
-													</Modal>
-												</>
-												:
-												<p>{siteDetail.features}</p>}
-										</>
-									}
-								</div>
-
-							</div> */}
-						</div>
-						:
-						<>
-							<img className="nonStarredScreenshot" src={siteDetail.screenshotUrl} alt={siteDetail.name} />
-							{/* <div className="onHoverInfo">
-								<h3 className="slideOutTitle">{siteDetail.name}</h3>
-								<div className="nonStarredFeatures">
-									{siteDetail.features !== null &&
-										<>
-											{siteDetail.features.length > maxLength ?
-												<>
-													<p>{`${siteDetail.features.substring(0, maxLength)}...`} <br /><button onClick={() => { getModal(index) }}>Read more</button></p>
-													<Modal
-														isOpen={modalIsOpen === index}
-														onRequestClose={closeModal}
-														ariaHideApp={false}
-													>
-														<div className="modalContainer">
-															<h3>{siteDetail.name} </h3>
-															<br />
-															<p>{siteDetail.features}</p>
-															<br />
-															<button className="modalClose" onClick={() => { closeModal() }}>Close</button>
-														</div>
-													</Modal>
-												</>
-												:
-												<p>{siteDetail.features}</p>}
-										</>
-									}
-								</div>
-
-							</div> */}
-
-						</>
-					}
-
-
-				</div>)
-
+				return (
+					<div className="nonStarredSiteItem" id={siteDetail.id} key={index}>
+						<img className="nonStarredScreenshot" src={siteDetail.screenshotUrl} alt={siteDetail.name} />
+					</div>
+				)
 			})}
 
 		</div>
